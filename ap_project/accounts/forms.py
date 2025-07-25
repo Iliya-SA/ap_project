774fc8dd7039f541d9bf886da0d6c71539f5461f
Iliya-SA <iliyasah85@gmail.com>
2025-07-25 20:39:49 +0330

    add signup and signin for the project in account folder

---
+++ ap_project/accounts/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data.get('first_name', '')
+        user.last_name = self.cleaned_data.get('last_name', '')
+        if commit:
+            user.save()
+        return user
+
+class SignInForm(AuthenticationForm):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
--- ap_project/products/admin.py
+++ ap_project/products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Product 
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin) :
+    list_display = ('name', 'brand', 'category', 'price', 'stock', 'rating')
+    search_fields = ('name', 'brand', 'category')
+    list_filter = ('category', 'brand', 'skin_types')
