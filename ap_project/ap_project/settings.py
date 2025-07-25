d21d0c9d89cb82f3d848810a41fe3e78ffaec305
Iliya-SA <iliyasah85@gmail.com>
2025-07-25 03:54:37 +0330

    Add MySQL Database

--- ap_project/ap_project/settings.py
+++ ap_project/ap_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -74,11 +76,17 @@ WSGI_APPLICATION = 'ap_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ap_project',
+        'USER': 'root',                 
+        'PASSWORD': '@iliya1385',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
     }
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
