556ac4a9622700d23302c9dd8f3bc51512c6028e
Iliya-SA <iliyasah85@gmail.com>
2025-07-27 19:26:39 +0330

    Add user profiles and create product detail pages and product commenting system

--- ap_project/products/admin.py
+++ ap_project/products/admin.py
@@ -3,6 +3,6 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin) :
-    list_display = ('name', 'brand', 'category', 'price', 'stock', 'rating')
+    list_display = ('name', 'brand', 'category', 'price', 'stock')
     search_fields = ('name', 'brand', 'category')
     list_filter = ('category', 'brand', 'skin_types')
--- ap_project/products/views.py
+++ ap_project/products/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Product, Comment
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView
 
-# Create your views here.
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['recent_comments'] = self.object.comments.order_by('-created_at')[:3]
+        return context
+
+class ProductCommentsView(ListView):
+    model = Comment
+    template_name = 'products/product_comments.html'
+    context_object_name = 'comments'
+
+    def get_queryset(self):
+        self.product = get_object_or_404(Product, pk=self.kwargs['pk'])
+        return Comment.objects.filter(product=self.product).order_by('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = self.product
+        return context
+
+class AddCommentView(LoginRequiredMixin, CreateView):
+    model = Comment
+    fields = ['text', 'rating']
+    template_name = 'products/add_comment.html'
+
+    def get_object(self, queryset=None):
+        return None  # جلوگیری از تلاش برای پیدا کردن Comment موجود
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user.profile
+        form.instance.product = get_object_or_404(Product, pk=self.kwargs['pk'])
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse_lazy('product-detail', kwargs={'pk': self.kwargs['pk']})
