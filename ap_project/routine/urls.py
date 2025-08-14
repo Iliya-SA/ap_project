67002fb526fed6a5c9a1d0f25af250a757e5f5ea
Sadix <pouya.bashar@gmail.com>
2025-08-14 18:45:44 +0330

    Add .gitignore and remove tracked __pycache__ files

--- ap_project/routine/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import quiz_view, my_routine
-
-urlpatterns = [
-    path('quiz/', quiz_view, name='quiz_api'),
-    path('my/', my_routine, name='my_routine'),
-]
