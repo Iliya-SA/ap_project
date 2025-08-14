daad3547d1178fe6663f60e42527af4a933b1828
Sadix <pouya.bashar@gmail.com>
2025-08-14 18:45:44 +0330

    Add .gitignore and remove tracked __pycache__ files

--- ap_project/context/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ContextData
-
-@admin.register(ContextData)
-class ContextDataAdmin(admin.ModelAdmin):
-    list_display = ['user', 'device_type', 'season', 'timestamp']
-    list_filter = ['device_type', 'season', 'timestamp']
--- ap_project/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ProductDetailView, ProductCommentsView, AddCommentView, toggle_favorite 
-
-urlpatterns = [
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product-detail'),
-    path('product/<int:pk>/comments/', ProductCommentsView.as_view(), name='product-comments'), 
-    path('product/<int:pk>/add-comment/', AddCommentView.as_view(), name='add-comment'),
-    path('favorite/<int:product_id>/', toggle_favorite, name='toggle-favorite'),
-]
