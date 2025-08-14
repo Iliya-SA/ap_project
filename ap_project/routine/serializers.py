b54653497687d5b2bc91fa9d9949945229acbdc3
Sadix <pouya.bashar@gmail.com>
2025-08-14 18:30:36 +0330

    Enhance routine serializers with product brand/price/image for richer responses

--- ap_project/routine/serializers.py
+++ ap_project/routine/serializers.py
@@ -1,26 +1,21 @@
+# routine/serializers.py
 from rest_framework import serializers
 from .models import RoutinePlan, RoutineStep
-from products.models import Product  # برای نمایش اطلاعات محصول
-
 
 class RoutineStepSerializer(serializers.ModelSerializer):
-    product_name = serializers.CharField(source='product.name', read_only=True)
-    
+    product_name = serializers.CharField(source="product.name", read_only=True)
+    product_brand = serializers.CharField(source="product.brand", read_only=True)
+    product_price = serializers.IntegerField(source="product.price", read_only=True)
+    product_image = serializers.ImageField(source="product.image", read_only=True)
+
     class Meta:
         model = RoutineStep
-        fields = ['step_name', 'product', 'product_name']
-
+        fields = ["step_name", "product", "product_name", "product_brand", "product_price", "product_image"]
 
 class RoutinePlanSerializer(serializers.ModelSerializer):
-    steps = RoutineStepSerializer(many=True)  # وصل کردن مراحل روتین (Nested)
-    
+    user = serializers.IntegerField(source="user.id", read_only=True)
+    steps = RoutineStepSerializer(many=True, read_only=True)
+
     class Meta:
         model = RoutinePlan
-        fields = ['id', 'user', 'plan_name', 'created_at', 'steps']
-
-    def create(self, validated_data):
-        steps_data = validated_data.pop('steps')
-        routine = RoutinePlan.objects.create(**validated_data)
-        for step in steps_data:
-            RoutineStep.objects.create(routine=routine, **step)
-        return routine
+        fields = ["id", "user", "plan_name", "created_at", "steps"]
