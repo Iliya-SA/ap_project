e8f50840013623a85eca9e3e7200788ef4a6f4d8
Sadix <pouya.bashar@gmail.com>
2025-08-14 18:29:44 +0330

    Configure DRF defaults: TokenAuthentication + IsAuthenticated

--- ap_project/ap_project/settings.py
+++ ap_project/ap_project/settings.py
@@ -140,3 +140,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
